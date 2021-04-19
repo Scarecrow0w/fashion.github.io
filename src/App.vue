3a96c6aa654913ee3c40733e451a6687d6ed56f3
Scarecrow0w <daniil6547@ya.ru>
2021-04-19 17:01:57 +0300

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+<Navbar />
+<Header />
+<section class="flex flex-wrap">
+    <Articles />
+    <SideInfo />
+</section>  
+<Footer />
+</template>
+
+<script setup>
+import Navbar from './components/Navbar.vue'
+import Header from './components/Header.vue'
+import Articles from './components/Articles.vue'
+import SideInfo from './components/SideInfo.vue'
+import Footer from './components/Footer.vue'
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style scoped>
+    section {
+        padding-top: 60px;
+    }
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        fgold: '#774D00',
+        tgold: '#c9a96e'
+      }
+    },
+    fontFamily: {
+      'sans': 'PT, sans-serif',
+      'serif': 'PT, serif'
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
